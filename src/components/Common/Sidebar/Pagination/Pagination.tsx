76292de82d870a961f34419e000e6ccd9b351ef3
Margo <margokomilfo@mail.ru>
2020-10-04 00:16:43 +0200

    55,93 - pagination is done!

---
+++ src/components/Common/Sidebar/Pagination/Pagination.tsx
@@ -0,0 +1,47 @@
+import {UserType} from "../../../../redux/allUsersReduser";
+import React, {useState} from "react";
+import axios from "axios";
+import s from "../Users/Users.module.css";
+
+type PaginationPropsType = {
+    totalCountPageArr: Array<number>
+    setUsers: (users: Array<UserType>) => void
+    setCurrentPage: (page: number) => void
+    currentPage: number
+    totalCountPage: number
+}
+
+const Pagination: React.FC<PaginationPropsType> = ({totalCountPageArr, setUsers, setCurrentPage, currentPage, totalCountPage}) => {
+
+    let onChangeCurrentPage = (page: number) => {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=24&page=${page}`).then(response => {
+            setUsers(response.data.items)
+        })
+        setCurrentPage(page)
+    }
+
+    let portionSize = 20
+    let portionCount = Math.ceil(totalCountPage/portionSize)
+    let [portionNumber, setPortionNumber] = useState<number>(1)
+    let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1
+    let rightPortionPageNumber = portionNumber * portionSize
+
+    let onPrevBtn = () => {setPortionNumber(portionNumber - 1)}
+    let onNextBtn = () => {setPortionNumber(portionNumber + 1)}
+    return (
+        <>
+            {portionNumber > 1 && <button onClick={onPrevBtn}>PREV</button>}
+            {totalCountPageArr
+                .filter(p => p >= leftPortionPageNumber && p<= rightPortionPageNumber)
+                .map(p => {
+                    return <span key={p} className={currentPage === p ? s.currentPage : s.uncurrenPage} onClick={() => {
+                        onChangeCurrentPage(p)
+                    }}>{p}</span>
+                })}
+            {portionCount > portionNumber && <button onClick={onNextBtn}>NEXT</button>}
+        </>
+    )
+
+}
+
+export default Pagination
--- src/components/Common/Sidebar/Users/Users.tsx
+++ src/components/Common/Sidebar/Users/Users.tsx
@@ -2,7 +2,8 @@ import React, {useState} from "react"
 import {UserType} from "../../../../redux/allUsersReduser"
 import s from './Users.module.css'
 import axios from 'axios'
-import noPhoto from '../../../../img/noPhoto.png'
+import Pagination from "../Pagination/Pagination";
+import User from "./User";
 
 type UsersPropsType = {
     follow: (id: string) => void
@@ -13,7 +14,6 @@ type UsersPropsType = {
     totalCountUsers: number
 }
 
-
 const Users: React.FC<UsersPropsType> = ({follow, unfollow, setUsers, users, setUsersCount, totalCountUsers}) => {
 
     let [currentPage, setCurrentPage] = useState<number>(1)
@@ -21,7 +21,7 @@ const Users: React.FC<UsersPropsType> = ({follow, unfollow, setUsers, users, set
 //----------------axios-------------------------------
     !users.length &&
     //count, page
-    axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=15&page=${currentPage}`).then(response => {
+    axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=24&page=${currentPage}`).then(response => {
         setUsers(response.data.items)
         setUsersCount(response.data.totalCount)
     })
@@ -35,71 +35,26 @@ const Users: React.FC<UsersPropsType> = ({follow, unfollow, setUsers, users, set
 
     return (
         <div className={s.usersStyle}>
+            <div className={s.pagination}>
+                <Pagination totalCountPageArr={totalCountPageArr}
+                            setCurrentPage={setCurrentPage}
+                            setUsers={setUsers}
+                            currentPage={currentPage}
+                            totalCountPage={totalCountPage}/>
+            </div>
             <div className={s.users}>{
-                users.map(u => <User name={u.name} followed={u.followed} follow={follow} unfollow={unfollow}
-                                     id={u.id}/>)}
+                users.map(u => <User name={u.name}
+                                     followed={u.followed}
+                                     follow={follow}
+                                     unfollow={unfollow}
+                                     id={u.id}/>
+                                     )
+                }
             </div>
-            <Pagination totalCountPageArr={totalCountPageArr} setCurrentPage={setCurrentPage} setUsers={setUsers} currentPage={currentPage}/>
         </div>
 
     )
 }
 
-type UserPropsType = {
-    name: string
-    followed: boolean
-    follow: (id: string) => void
-    unfollow: (id: string) => void
-    id: string
 
-}
-const User: React.FC<UserPropsType> = ({name, followed, follow, unfollow, id}) => {
-    return (
-        <div className={s.user}>
-            <div className={s.photo}>
-                <img src={noPhoto} alt="" className={s.src}/>
-            </div>
-            <div className={s.name}>{name}</div>
-            <div>
-                {followed ? <button onClick={() => {
-                    follow(id)
-                }}>Follow</button> : <button onClick={() => {
-                    unfollow(id)
-                }}>Unfollow</button>}
-            </div>
-        </div>
-    )
-}
-
-
-type PaginationPropsType = {
-    totalCountPageArr: Array<number>
-    setUsers: (users: Array<UserType>) => void
-    setCurrentPage: (page:number) => void
-    currentPage: number
-}
-
-const Pagination: React.FC<PaginationPropsType> = ({totalCountPageArr, setUsers, setCurrentPage, currentPage}) => {
-
-    let onChangeCurrentPage = (page: number) => {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=15&page=${page}`).then(response => {
-            setUsers(response.data.items)
-        })
-        setCurrentPage(page)
-    }
-
-    return (
-        <div className={s.pagination}>
-            {
-                totalCountPageArr.map(p => {
-                    // @ts-ignore
-                    return <span className={currentPage === p ? s.currentPage : s.uncurrenPage} onClick={() => {
-                        onChangeCurrentPage(p)
-                    }}>{p}</span>
-                })
-            }
-        </div>
-    )
-
-}
 export default Users
