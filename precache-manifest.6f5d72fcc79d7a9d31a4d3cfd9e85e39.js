ec0c49ad6a7dbbb3f098af884491c2ae0336e543
Margokomilfo-dev <margokomilfo@mail.ru>
2020-12-30 00:09:50 +0100

    Updates

---
+++ precache-manifest.6f5d72fcc79d7a9d31a4d3cfd9e85e39.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "befbd51285b2a6e4a55ceb665f5da00c",
+    "url": "/it-incubator-socialnetwork/index.html"
+  },
+  {
+    "revision": "422ff7a40b8209967586",
+    "url": "/it-incubator-socialnetwork/static/css/main.607ea80f.chunk.css"
+  },
+  {
+    "revision": "fa3653ea208f591635f0",
+    "url": "/it-incubator-socialnetwork/static/js/2.cd67394a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/it-incubator-socialnetwork/static/js/2.cd67394a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "422ff7a40b8209967586",
+    "url": "/it-incubator-socialnetwork/static/js/main.e1f05c92.chunk.js"
+  },
+  {
+    "revision": "81022fc1734c02f5d5bc",
+    "url": "/it-incubator-socialnetwork/static/js/runtime-main.fd24b4a2.js"
+  },
+  {
+    "revision": "b6325a6d10cb66d7bf689c831b821a60",
+    "url": "/it-incubator-socialnetwork/static/media/logo-job.b6325a6d.png"
+  },
+  {
+    "revision": "f669fc504820b9f504320ecbad03b57c",
+    "url": "/it-incubator-socialnetwork/static/media/preloader.f669fc50.svg"
+  }
+]);
